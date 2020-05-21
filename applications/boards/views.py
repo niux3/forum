fc35b699b268af83b3ebff81ba565a3de991a4fc
Renaud Bourdeau <renaudbourdeau@gmail.com>
2020-05-22 01:20:17 +0200

    feat(boards): create home view and init admin

--- applications/boards/views.py
+++ applications/boards/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 
 def index(request):
-    return HttpResponse('boards !')
+    context = {
+        'boards': Board.objects.all()
+    }
+    return render(request, 'home.html', context)
