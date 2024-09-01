ba731593cc611a012cab95c1cde41d413cbbe6a6
niux3 <renaudbourdeau@gmail.com>
2024-09-01 23:40:16 +0200

    feat: command create fake users & install faker

---
+++ apps/registration/management/commands/create_user.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from faker import Faker
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        admin = User.objects.create_user('admin', password='admin')
+        admin.is_superuser = True
+        admin.is_staff = True
+        admin.save()
+        print('admin created')
+
+        fake = Faker('fr-FR')
+        for _ in range(25):
+            profile = fake.simple_profile()
+            user = User.objects.create_user(profile['username'], password='123456')
+            user.email = profile['mail']
+            user.save()
+            print('*' * 80)
+            print(f'username : {profile["username"]}')
+            print(f'password : 123456')
