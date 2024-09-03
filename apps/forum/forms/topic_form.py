27f247abf61ddf925abdc58b038a128e45620df7
niux3 <renaudbourdeau@gmail.com>
2024-09-03 12:42:33 +0200

    feat: change form and view create topic

--- apps/forum/forms/topic_form.py
+++ apps/forum/forms/topic_form.py
@@ -1,17 +1,22 @@
 from django import forms
-from forum.models import Topic
+from forum.models import Topic, Board
 
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = [
+            'forum',
             'subject',
             'message',
         ]
         widgets = {
             'subject': forms.TextInput(attrs={'class': 'form-control'}),
         }
-    message = forms.CharField(max_length=4000, widget=forms.Textarea(), help_text="la longueur max. est de 4000 caractères")
 
-
+    forum = forms.ModelChoiceField(queryset=Board.objects.all(), widget=forms.Select(attrs={
+        'class': 'form-control'
+    }))
+    message = forms.CharField(max_length=4000, widget=forms.Textarea(attrs={
+        'class': 'form-control'
+    }), help_text="la longueur max. est de 4000 caractères")
--- apps/forum/views/topic_create.py
+++ apps/forum/views/topic_create.py
@@ -12,24 +12,11 @@ class TopicCreate(LoginRequiredMixin, CreateView):
     template_name = 'forum/create.html'
     form_class = TopicForm
 
-    def setup(self, request, *args, **kwargs):
-        super().setup(request, *args, **kwargs)
-        self.board = get_object_or_404(
-            Board,
-            pk=self.kwargs.get('id'), 
-            slug=self.kwargs.get('slug')
-        )
-
-    def get_context_data(self, *args, **kwargs):
-        ctx = super().get_context_data(*args, **kwargs)
-        ctx['board'] = self.board
-        return ctx
-
     def form_valid(self, form):
         user = self.request.user
         topic = form.save(commit=False)
         topic.slug = slugify(form.cleaned_data.get('subject'))
-        topic.board = self.board
+        topic.board = form.cleaned_data.get('forum')
         topic.starter = user
         topic.save()
 
@@ -40,10 +27,11 @@ class TopicCreate(LoginRequiredMixin, CreateView):
         )
         
         messages.success(self.request, "Votre sujet vient d'être bien pris en compte.")
-
-        return redirect('forum:topics', **{
-            'slug': self.kwargs.get('slug'),
-            'id': self.kwargs.get('id')
+        return redirect('forum:show_topic', **{
+            'slug': form.cleaned_data.get('forum').slug,
+            'id': form.cleaned_data.get('forum').id,
+            'id_post': topic.id,
+            'slug_post': topic.slug
         })
 
     def form_invalid(self, form):
