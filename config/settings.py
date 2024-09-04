640a840280bbd9d074d07d0b499e75494a5a1ff7
niux3 <renaudbourdeau@gmail.com>
2024-09-04 15:57:45 +0200

    feat: reorganize files templates and static

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            BASE_DIR / 'apps' / 'core' /'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'apps' / 'core' /'static',
 ]
 
 # account
