b6df1d8d92e300754e8af7f96c2cf13162e10549
niux3 <renaudbourdeau@gmail.com>
2024-09-04 10:44:36 +0200

    feat: get active boards urls

---
+++ apps/forum/context_processors.py
@@ -0,0 +1,34 @@
+from django.urls import reverse_lazy
+
+
+def boards_urls(request):
+    args = {
+        'index': {
+            'name': 'forum:index',
+            'params': None
+        },
+        'last_topics': {
+            'name': 'forum:last_topics',
+            'params': 'derniers-sujets'
+        },
+        'last_posts': {
+            'name': 'forum:last_posts',
+            'params': 'dernieres-reponses'
+        },
+        'my_topics': {
+            'name': 'forum:last_topics',
+            'params': 'mes-sujets'
+        },
+        'my_posts': {
+            'name': 'forum:last_posts',
+            'params': 'mes-reponses'
+        },
+    }
+    urls = {
+        k: reverse_lazy(v['name'], kwargs={ 'categories': v['params'] }) if v['params'] is not None else reverse_lazy(v['name']) 
+        for k, v in args.items()
+    }
+
+    return {
+        'boards_urls': urls
+    }
