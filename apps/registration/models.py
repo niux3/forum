98cba21d3addde28ba4dbe9f80cca22f60b75f34
niux3 <renaudbourdeau@gmail.com>
2024-09-05 13:29:47 +0200

    feat: add profile model

--- apps/registration/models.py
+++ apps/registration/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    website = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
+    facebook = models.URLField(null=True, blank=True)
+    linkedin = models.URLField(null=True, blank=True)
+    newsletter = models.BooleanField(default=False)
+    private_message = models.BooleanField(default=True)
+    show_email = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
