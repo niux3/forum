2f86f0c09c7cebfcc06465c2b3f0ac402ee3cba8
niux3 <renaudbourdeau@gmail.com>
2024-09-19 16:44:37 +0200

    feat: upgrade search

--- apps/forum/forms/advanced_search_form.py
+++ apps/forum/forms/advanced_search_form.py
@@ -5,9 +5,9 @@ from forum.models import Board
 
 class AdvancedSeachForm(forms.Form):
     boards = forms.ModelChoiceField(label='catégories', queryset=Board.objects.all(), required=False)
-    # search_in = forms.MultipleChoiceField(label='Rechercher dans', choices=(
-        # ('', 'choisir une option'),
-        # ('topics', 'sujets'),
-        # ('topics_posts', 'sujets et réponses')
-    # ), widget=forms.Select(attrs={'class': 'form-check-input'}), required=False, initial=0)
+    search_in = forms.MultipleChoiceField(label='Rechercher dans', choices=(
+        ('', 'choisir une option'),
+        ('topics', 'sujets'),
+        ('posts', 'réponses')
+    ), widget=forms.Select(attrs={'class': 'form-check-input'}), required=False, initial=0)
     users = forms.ModelChoiceField(label="membres", queryset=User.objects.all(), required=False)
--- apps/forum/views/topics_search.py
+++ apps/forum/views/topics_search.py
@@ -9,15 +9,16 @@ class TopicSearch(ListView):
     template_name = 'forum/search_topics.html'
 
     def get_queryset(self):
-        additionnal = None
-        params = {
-            'subject__contains': self.request.GET.get('q')
-        }
+        params = {}
         if self.request.GET.get('boards') != '':
             params['board'] = self.request.GET.get('boards')
         if self.request.GET.get('users') != '':
             params['starter'] = self.request.GET.get('users')
-        if self.request.GET.get('search_in') != '' and self.request.GET.get('search_in') == 'topics_posts':
-            additionnal = Post.objects.filter(message__contains=self.request.GET.get('q'))
-        queryset = Topic.objects.filter(**params)
-        return queryset if additionnal is not None else queryset
+
+        if self.request.GET.get('search_in') != '' and self.request.GET.get('search_in') == 'posts':
+            params["posts__message__contains"] = self.request.GET.get('q')
+            queryset = Topic.objects.filter(**params)
+        else:
+            params['subject__contains'] = self.request.GET.get('q')
+            queryset = Topic.objects.filter(**params)
+        return queryset
