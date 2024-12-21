a934c202b378e312064282b428a265165b2b443c
niux3 <renaudbourdeau@gmail.com>
2024-12-21 14:22:02 +0100

    feat: delete mp done

--- apps/forum/views/private_message_delete.py
+++ apps/forum/views/private_message_delete.py
@@ -7,8 +7,13 @@ from forum.models import PrivateMessage
 
 class PrivateMessageDelete(LoginRequiredMixin, DeleteView):
     model = PrivateMessage
-    success_url = reverse_lazy('forum:private_message_list')
 
     def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs) # no template confirm
+
+    def post(self, request, *args, **kwargs):
+        count_message = PrivateMessage.objects.filter(to_user=request.user.id).count()
+        url = 'forum:private_message_list' if count_message > 1 else 'forum:index'
+        self.success_url = reverse_lazy(url)
         messages.success(self.request, "Votre message a bien été supprimé")
         return self.delete(request, *args, **kwargs)
